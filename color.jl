7ff3a8357d84853905d22c3d6a9f1b97b0712f08
daviderivaa <davide.riva12@studenti.unimi.it>
2025-03-12 12:03:14 +0100

    Add HdrImage struct

--- color.jl
+++ color.jl
@@ -6,3 +6,15 @@ struct Color
     # Costruttore con valori di default
     Color(r=0.0, g=0.0, b=0.0) = new(r, g, b)
 end
+
+struct HdrImage
+    width::Int
+    height::Int
+    pixels::Vector{Color}
+
+    # Costruttore personalizzato con valori di default
+    function HdrImage(width::Int=0, height::Int=0)
+        pixels = [Color() for _ in 1:(width * height)]
+        new(width, height, pixels)
+    end
+end
