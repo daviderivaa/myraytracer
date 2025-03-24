1eede18832c80c03f74b835aaf969102a5049b67
Alberto Lazzeri <albilazze2002@gmail.com>
2025-03-24 16:43:44 +0100

    file-path correction in main

--- main.jl
+++ main.jl
@@ -2,6 +2,6 @@ using Pkg
 Pkg.activate("myRayTracing")
 using myRayTracing #Fino a qua non toccare che serve a importare classi e librerie correttamente
 
-format, width, height, endianness, pixel_data = _read_pfm("/home/davide/Fotorealistiche/reference_le.pfm")
+format, width, height, endianness, pixel_data = _read_pfm("./reference_le.pfm")
 
 println(format, "\n", width, "\n", height, "\n", endianness, "\n", pixel_data)
