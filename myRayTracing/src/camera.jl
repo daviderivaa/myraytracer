96554ec397a5ddf880fd8cb7ea441c2adee662af
daviderivaa <davide.riva12@studenti.unimi.it>
2025-04-16 11:20:29 +0200

    adding fire_ray method

--- myRayTracing/src/camera.jl
+++ myRayTracing/src/camera.jl
@@ -5,18 +5,18 @@ abstract type Camera
 end
 
 function fire_ray(cam::Camera, u, v)
+    error("Metodo fire_ray non implementato per $(typeof(cam))")
 end
 
 #Defining substructs
 #Orthogonal Camera
 struct OrthogonalCamera <: Camera
 
-    distance::Float64
     aspect_ratio::Float64
     transfromation::Transformation
 
-    function OrthogonalCamera(distance, aspect_ratio, transfromation::Transformation)
-        new(distance, aspect_ratio, transfromation)
+    function OrthogonalCamera(aspect_ratio, transfromation::Transformation)
+        new(aspect_ratio, transfromation)
     end
 
 end
@@ -24,8 +24,8 @@ end
 function fire_ray(cam::OrthogonalCamera, u, v)
 
     origin = Point(-1.0, (1.0 - 2 * u) * cam.aspect_ratio, 2 * v - 1)
-    direction = VEC_X
-    return Ray(origin=origin, dir=direction, tmin=1.0e-5).transform(cam.transformation)
+    direction = Vec(1.0, 0.0, 0.0)
+    return cam.transformation*Ray(origin=origin, dir=direction, tmin=1.0e-5)
 
 end
 
@@ -43,5 +43,18 @@ struct PerspectiveCamera <: Camera
 end
 
 function fire_ray(cam::PerspectiveCamera, u, v)
-    # codice specifico per PerspectiveCamera
+    
+    origin = Point(-cam.distance, 0.0, 0.0)
+    direction = Vec(cam.distance, (1.0 - 2 * u) * cam.aspect_ratio, 2 * v - 1)
+    return cam.transformation*Ray(origin=origin, dir=direction, tmin=1.0e-5)
+
+end
+
+function aperture_deg(cam::PerspectiveCamera)
+
+    """Compute the aperture of the camera in degrees
+
+    The aperture is the angle of the field-of-view along the horizontal direction (Y axis)"""
+    return 2.0 * math.atan(cam.distance / cam.aspect_ratio) * 180.0 / 3.14159265359
+
 end
