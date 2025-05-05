e7c58412a5d44dc1ac4fa02f3e4a0e95e02e95cd
Alberto Lazzeri <albilazze2002@gmail.com>
2025-05-05 10:30:42 +0200

    fixing docstrings

--- myRayTracing/src/hitrecord.jl
+++ myRayTracing/src/hitrecord.jl
@@ -3,6 +3,12 @@
 """
 struct HitRecord
     Creates a 3D unit sphere centered in the origin of the axes. A transfromation can be passed to translate and transform it into an ellipsoid
+
+    world_point::Point --> 3D point where the intersection occurred
+    normal::Normal --> surface normal vector
+    surface_point::Vec2d --> coordinates of the intersection
+    t::Float64 --> parameter associated with the intersection
+    ray::Ray --> light ray that caused the intersection
 """
 struct HitRecord
 
