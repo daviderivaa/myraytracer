0d7abc74c0bec7f474f32703f012e96b8613f4b1
Alberto Lazzeri <albilazze2002@gmail.com>
2025-05-09 12:28:06 +0200

    generalize demo with different camera_type and POVs

--- demo.jl
+++ demo.jl
@@ -8,6 +8,37 @@ using myRayTracing
 
 include("pfm2png.jl")
 
+#Error definition
+"""
+throws a message if ARGS is incorrect
+"""
+struct InvalidARGS <: Exception
+    msg::String
+end
+
+
+if length(ARGS) != 1
+    throw(InvalidARGS("Required julia demo.jl <camera_type>\nWrite perspective or orthogonal"))
+end
+
+
+if ARGS[1] == "perspective"
+    pfm_filename_and_path = "./demo/demo_perspective.pfm"
+    filename = "demo_perspective"
+    rot1 = rotation("z", π/6)
+    Cam = PerspectiveCamera(-1.0, 16.0/9.0, rot1(traslation(Vec(1.0, 0.0, 0.0))))
+
+elseif ARGS[1] == "orthogonal"
+    pfm_filename_and_path = "./demo/demo_orthogonal.pfm"
+    filename = "demo_orthogonal"
+    rot1 = rotation("z", -π/15)
+    rot2 = rotation("y", -π/18)
+    Cam = OrthogonalCamera(16.0/9.0, rot1(rot2(traslation(Vec(-2.0, 0.0, 0.0)))))
+
+else
+    throw(InvalidARGS("Error in ARGS: in <camera_type> write perspective or orthogonal"))
+end
+
 w = World()
 
 coords = [-0.5,0.5]
@@ -24,9 +55,8 @@ s2 = Sphere(trasl2(scaling(0.1)))
 add_shape!(w,s1)
 add_shape!(w,s2)
 
-PC = PerspectiveCamera(-1.0, 16.0/9.0, traslation(Vec(1.0, 0.0, 0.0)))
 img = HdrImage(1600,900)
-ITC = ImageTracer(img, PC)
+IT = ImageTracer(img, Cam)
 
 function func(ray)
     if ray_intersection(w, ray) !== nothing
@@ -36,13 +66,13 @@ function func(ray)
     end
 end
 
-fire_all_rays!(ITC, func)
+fire_all_rays!(IT, func)
 
-open("./demo_perspective.pfm", "w") do io
-    write_pfm(io, ITC.img)
+open(pfm_filename_and_path, "w") do io
+    write_pfm(io, IT.img)
 end
 
-convert_pfm_to_png("./demo_perspective.pfm","demo_perspective")
+convert_pfm_to_png(pfm_filename_and_path,filename)
 
 #format, width, height, endianness, pixel_data = read_pfm("./demo_perspective.pfm")
 #alpha, gamma, output_file_name, output_file_format = read_user_input()
