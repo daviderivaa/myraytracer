57a0abd3875f61fbef7b8fd443bd290ed7474a80
Alberto Lazzeri <albilazze2002@gmail.com>
2025-05-15 12:10:14 +0200

    try demo

--- demo.jl
+++ demo.jl
@@ -41,38 +41,38 @@ for angle in 0:359
         throw(InvalidARGS("Error in ARGS: in <camera_type> write perspective or orthogonal"))
     end
 
-    w = World()
+    # w = World()
 
-    coords = [-0.5,0.5]
-    for x in coords, y in coords, z in coords
-        trasl = traslation(Vec(x,y,z)) #put sphere in the correct position
-        s = Sphere(trasl(scaling(0.1))) #creates a sphere with radius = 0.1
-        add_shape!(w, s)
-    end
+    # coords = [-0.5,0.5]
+    # for x in coords, y in coords, z in coords
+    #     trasl = traslation(Vec(x,y,z)) #put sphere in the correct position
+    #     s = Sphere(trasl(scaling(0.1))) #creates a sphere with radius = 0.1
+    #     add_shape!(w, s)
+    # end
 
-    trasl1 = traslation(Vec(0.0, 0.0, -0.5))
-    trasl2 = traslation(Vec(0.0, 0.5, 0.0))
-    s1 = Sphere(trasl1(scaling(0.1)))
-    s2 = Sphere(trasl2(scaling(0.1)))
-    add_shape!(w,s1)
-    add_shape!(w,s2)
+    # trasl1 = traslation(Vec(0.0, 0.0, -0.5))
+    # trasl2 = traslation(Vec(0.0, 0.5, 0.0))
+    # s1 = Sphere(trasl1(scaling(0.1)))
+    # s2 = Sphere(trasl2(scaling(0.1)))
+    # add_shape!(w,s1)
+    # add_shape!(w,s2)
 
-    img = HdrImage(1600,900)
-    IT = ImageTracer(img, Cam)
+    # img = HdrImage(1600,900)
+    # IT = ImageTracer(img, Cam)
 
-    function func(ray)
-        if ray_intersection(w, ray) !== nothing
-            return RGB(1.0, 1.0, 1.0)
-        else
-            return RGB(0.0, 0.0, 0.0)
-        end
-    end
+    # function func(ray)
+    #     if ray_intersection(w, ray) !== nothing
+    #         return RGB(1.0, 1.0, 1.0)
+    #     else
+    #         return RGB(0.0, 0.0, 0.0)
+    #     end
+    # end
 
-    fire_all_rays!(IT, func)
+    # fire_all_rays!(IT, func)
 
-    open(pfm_filename_and_path, "w") do io
-        write_pfm(io, IT.img)
-    end
+    # open(pfm_filename_and_path, "w") do io
+    #     write_pfm(io, IT.img)
+    # end
 
     convert_pfm_to_png(pfm_filename_and_path,filename)
 end
@@ -94,21 +94,22 @@ for (i, file) in enumerate(files)
     end
 end
 
-# Usa FFmpeg per creare la GIF con una sequenza numerica
-cmd = `ffmpeg -framerate 10 -i './demo/img_%d.png' -vf "fps=10,scale=320:-1:flags=lanczos" $(ARGS[1] * ".gif")`
+#GIF parametere
+#cmd = `ffmpeg -framerate 10 -i './demo/img_%d.png' -vf "fps=10,scale=320:-1:flags=lanczos" $(ARGS[1] * ".gif")`
+cmd = `ffmpeg -framerate 36 -i './demo/img_%d.png' -vf "fps=36,scale=640:-1:flags=lanczos" -loop 0 $(ARGS[1] * ".gif")`
 
 # Esegui il comando
 println("Eseguo comando: ", cmd)
 run(cmd)
 
-# Elimina i file numerati e anche i file .pfm corrispondenti dopo aver creato la GIF
-for (i, _) in enumerate(files)
-    # Elimina il file PNG
-    rm("./demo/img_$(i).png")
+# # Elimina i file numerati e anche i file .pfm corrispondenti dopo aver creato la GIF
+# for (i, _) in enumerate(files)
+#     # Elimina il file PNG
+#     rm("./demo/img_$(i).png")
     
-    # Elimina il file PFM con lo stesso nome
-    pfm_file = "./demo/img_$(i).pfm"
-    if isfile(pfm_file)  # Verifica se il file .pfm esiste
-        rm(pfm_file)
-    end
-end
+#     # Elimina il file PFM con lo stesso nome
+#     pfm_file = "./demo/img_$(i).pfm"
+#     if isfile(pfm_file)  # Verifica se il file .pfm esiste
+#         rm(pfm_file)
+#     end
+# end
