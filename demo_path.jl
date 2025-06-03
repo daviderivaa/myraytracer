fc03fd3393b12c05767ef80a75d19565e97bd993
Alberto Lazzeri <albilazze2002@gmail.com>
2025-06-03 11:04:41 +0200

    Fix antialiasing and update profiling

--- demo_path.jl
+++ demo_path.jl
@@ -7,6 +7,7 @@ using Colors
 using myRayTracing
 using LinearAlgebra
 using Profile
+using PProf
 
 include("pfm2png.jl")
 
@@ -30,7 +31,7 @@ if ARGS[1] == "perspective"
     angle_y = parse(Float64, ARGS[3])
     rot1 = rotation("z", angle_z*π/180.0)
     rot2 = rotation("y", angle_y*π/180.0)
-    Cam = PerspectiveCamera(1.0, 16.0/9.0, rot1(rot2(traslation(Vec(1.0, 0.0, 0.0)))))
+    Cam = PerspectiveCamera(6.0, 16.0/9.0, rot1(rot2(traslation(Vec(-1.0, 0.0, 0.0)))))
 
 elseif ARGS[1] == "orthogonal"
     path = "./demo_path/"
@@ -73,13 +74,13 @@ material5 = Material(DiffuseBRDF(pig5, 0.5), pig5)
 
 s = Sphere(traslation(Vec(0.0, 0.0, -0.7))(scaling(0.3)), Material(SpecularBRDF(UniformPigment(RGB(1.0, 0.0, 0.0)))))
 s1 = Sphere(traslation(Vec(0.0, -1.3, -0.5))(scaling(0.5)), Material(DiffuseBRDF(UniformPigment(RGB(1.0, 1.0, 0.0)))))
-sky = Sphere(scaling(15.0), Material(DiffuseBRDF(UniformPigment(RGB(0.58, 0.56, 0.4)), 0.0), UniformPigment(RGB(0.58, 0.56, 0.4))))
+sky = Sphere(scaling(15.0), Material(DiffuseBRDF(UniformPigment(RGB(0.58, 0.56, 0.6)), 0.0), UniformPigment(RGB(0.58, 0.56, 0.6))))
 #sky = Sphere(scaling(10.0), Material(DiffuseBRDF(pigsky), pigsky))
 p2 = Plane(traslation(Vec(0.0, 0.0, -1.0)), material1)
-#add_shape!(w, s)
-#add_shape!(w, s1)
+add_shape!(w, s)
+add_shape!(w, s1)
 add_shape!(w, sky)
-#add_shape!(w, p2)
+add_shape!(w, p2)
 
 img = HdrImage(1600,900)
 IT = ImageTracer(img, Cam)
@@ -90,11 +91,9 @@ RND = PathTracer(w, RGB(0.0, 0.0, 0.0), pcg, 2, 3, 2)
 
 enable_profile = "--profile" in ARGS
 if enable_profile
-    Profile.clear()
-    @profile fire_all_rays!(IT, RND)
-    Profile.print()
+    @pprof fire_all_rays!(IT, RND, pcg, 4)
 else
-    val, t, bytes, gctime, gcstats = @timed fire_all_rays!(IT, RND)
+    val, t, bytes, gctime, gcstats = @timed fire_all_rays!(IT, RND, pcg, 4)
     println("Profiling fire_all_rays method:\nTime: $t s\nAllocated memory: $(bytes/1_000_000) MB\nGC: $gctime s")
     println("For a complete profiling use --profile flag")
 end
@@ -103,4 +102,4 @@ open(pfm_filename_and_path, "w") do io
     write_pfm(io, IT.img)
 end
 
-convert_pfm_to_png(path, pfm_filename_and_path, filename, 1.0)
+convert_pfm_to_png(path, pfm_filename_and_path, filename, 0.3)
